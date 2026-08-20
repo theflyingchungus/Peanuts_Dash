3e95c2faa832ad54ca5476cb49fd3eb1cc9dd9ef
theflyingchungus <suhaibster@gmail.com>
2026-08-20 21:48:50 +0800

    Added level 7

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 @onready var level_label: Label = $HUD/Panel/LevelLabel
 @onready var fade: ColorRect = $HUD/Fade
 
-var level: int = 1
+var level: int = 7
 var current_level_root: Node = null
 var entry_spawn_point: String = "FromPreviousLevel"
 
