f2e551284f168fe1eff92d6df7340e703f737d4e
theflyingchungus <suhaibster@gmail.com>
2026-08-21 15:31:51 +0800

    Fixed the camera boundary issues. Added secret levels

---
+++ scripts/camera_zone.gd
@@ -0,0 +1,19 @@
+# CameraZone.gd — attach to an Area2D
+extends Area2D
+
+@export var limit_left: int
+@export var limit_top: int
+@export var limit_right: int
+@export var limit_bottom: int
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		var camera = body.find_child("Camera2D", true, false)
+		if camera:
+			camera.limit_left = limit_left
+			camera.limit_top = limit_top
+			camera.limit_right = limit_right
+			camera.limit_bottom = limit_bottom
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 @onready var level_label: Label = $HUD/Panel/LevelLabel
 @onready var fade: ColorRect = $HUD/Fade
 
-var level: int = 8
+var level: int = 9
 var current_level_root: Node = null
 var entry_spawn_point: String = "FromPreviousLevel"
 
@@ -34,17 +34,54 @@ func _load_level(level_number: int) -> void:
 	# Fade in
 	await _fade(0.0)
 
+func _load_level_secret(level_number: int) -> void:
+	# Fade out
+	await _fade(1.0)
+	
+	if current_level_root:
+		current_level_root.queue_free()
+		
+	# Change Level
+	var level_path = "res://level_scenes/level%ssecret.tscn" % level_number
+	current_level_root = load(level_path).instantiate()
+	add_child(current_level_root)
+	current_level_root.name = "LevelRoot"
+	_setup_level(current_level_root)
+	
+	# Fade in
+	await _fade(0.0)
+
 func _setup_level(level_root: Node) -> void:
-	# Connect Exit
+	# Connect Exit 1
 	var exit = level_root.get_node_or_null("Exit")
 	if exit:
 		exit.body_entered.connect(_on_exit_body_entered)
 	
+	# Connect Exit 2
+	var exit_2 = level_root.get_node_or_null("Exit2")
+	if exit_2:
+		exit_2.body_entered.connect(_on_exit_2_body_entered)
+
+	# Connect Exit Secret
+	var exit_s = level_root.get_node_or_null("ExitSecret")
+	if exit_s:
+		exit_s.body_entered.connect(_on_exit_s_body_entered)
+
 	# Connect Entry
 	var entry = level_root.get_node_or_null("Entry")
 	if entry:
 		entry.body_entered.connect(_on_entry_body_entered)
-		
+
+	# Connect Entry 2
+	var entry_2 = level_root.get_node_or_null("Entry2")
+	if entry_2:
+		entry_2.body_entered.connect(_on_entry_2_body_entered)
+
+	# Connect Entry Secret
+	var entry_s = level_root.get_node_or_null("EntrySecret")
+	if entry_s:
+		entry_s.body_entered.connect(_on_entry_s_body_entered)
+
 	var pit = level_root.get_node_or_null("Pit")
 	if pit:
 		pit.body_entered.connect(_on_pit_body_entered)
@@ -76,11 +113,41 @@ func _on_exit_body_entered(body: Node2D) -> void:
 		GameState.set_spawn_point(entry_spawn_point)
 		call_deferred("_load_level", level)
 
+func _on_exit_2_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		level += 1
+		body.lock_movement()
+		entry_spawn_point = "FromPreviousLevel2"
+		GameState.set_spawn_point(entry_spawn_point)
+		call_deferred("_load_level", level)
+
+func _on_exit_s_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.lock_movement()
+		entry_spawn_point = "FromPreviousLevel"
+		GameState.set_spawn_point(entry_spawn_point)
+		call_deferred("_load_level_secret", level)
+
 func _on_entry_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		level -= 1
 		body.lock_movement()
 		entry_spawn_point = "FromNextLevel"
+		GameState.set_spawn_point(entry_spawn_point)
+		call_deferred("_load_level", level)
+		
+func _on_entry_2_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		level -= 1
+		body.lock_movement()
+		entry_spawn_point = "FromNextLevel2"
+		GameState.set_spawn_point(entry_spawn_point)
+		call_deferred("_load_level", level)
+
+func _on_entry_s_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.lock_movement()
+		entry_spawn_point = "FromSecretLevel"
 		GameState.set_spawn_point(entry_spawn_point)
 		call_deferred("_load_level", level)
 
